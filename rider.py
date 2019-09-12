525bc8a87476831fe3467f274b122a7295eda324
Heidi Hinkel <ad-astra@comcast.net>
2019-09-12 15:13:33 -0400

    Parsing message string.

--- rider.py
+++ rider.py
@@ -1,14 +1,22 @@
 class Rider:
     
     message = None
+    number = None
+    fence = None
+    startTime = None
+    finishTime = None
 
     def __init__(self, msg):
         print("created")
         Rider.message = msg
-        self.number = None
-        self.fence = None
-        self.startTime = None
-        self.finishTime = None
-
+        
     def parseMessage(self, msg):
-        print(Rider.message.split(','))
+        dataString = msg[msg.find("\'")+1:msg.find("\'")]
+        print(dataString)
+        dataList = dataString.split(',')
+        print(dataList)
+        Rider.number = dataList[0]
+        Rider.fence = dataList[1]
+        Rider.startTime = dataList[2]
+        Rider.finishTime = dataList[3]
+        
